d716564b8500ccc4c07717b1a4b4baa5e7d56ea7
Umer Kang <umerkang66@gmail.com>
2022-07-01 19:04:22 +0500

    'updates'

--- src/events/orders/order-canceled-event.ts
+++ src/events/orders/order-canceled-event.ts
@@ -4,8 +4,10 @@ export interface OrderCanceledEvent {
   // we are share limited amount of information, because there is no need for extra information
   subject: Subjects.OrderCanceled;
   data: {
+    // orderId is needed for payments
     id: string;
     ticket: {
+      // order-ticketId is needed for tickets-service so that it unreserve it.
       id: string;
     };
   };
