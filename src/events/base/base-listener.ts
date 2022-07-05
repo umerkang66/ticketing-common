71683795849a9e0016a128c199424052acbf8ef4
umerkang66 <umerkang66@gmail.com>
2022-07-05 10:05:32 +0500

    updates

--- src/events/base/base-listener.ts
+++ src/events/base/base-listener.ts
@@ -13,7 +13,7 @@ export abstract class Listener<T extends Event> {
   protected abstract onMessage(data: T['data'], msg: Message): void;
   protected ackWait = 5 * 1000;
 
-  constructor(private client: Stan) {}
+  constructor(protected client: Stan) {}
 
   private subscriptionOptions(): SubscriptionOptions {
     // DURABLE NAME
