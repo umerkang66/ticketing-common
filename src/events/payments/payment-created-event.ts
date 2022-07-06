73106f14d5d6f790fa167808bcc3aaae864a533d
umerkang66 <umerkang66@gmail.com>
2022-07-06 21:45:06 +0500

    updates

--- src/events/payments/payment-created-event.ts
+++ src/events/payments/payment-created-event.ts
@@ -5,7 +5,7 @@ export interface PaymentCreatedEvent {
   data: {
     id: string;
     orderId: string;
-    // this is used to retrieve information from stripe about payment, we currently are not using it
+    // this is used to retrieve information from stripe about payment, we currently are not using it.
     stripeId: string;
   };
 }
